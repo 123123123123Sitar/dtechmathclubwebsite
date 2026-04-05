cc730ec8f6e7b6df0348e96de54079504d6ba2ff
RileyGood.XYZ <lihengliu787@gmail.com>
2026-04-04 22:44:52 -0700

    Vervel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { DpotdAuthProvider } from "./context/DpotdAuthContext";
 import "./index.css";
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DpotdAuthProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </DpotdAuthProvider>
   </React.StrictMode>,
